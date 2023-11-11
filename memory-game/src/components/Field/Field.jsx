f8a7e39e23320b3b7314121e602cd82778e3e0c4
Valentina Danilova <danilova.vv@phystech.edu>
2023-11-11 20:37:05 +0400

    add generate field helper

--- memory-game/src/components/Field/Field.jsx
+++ memory-game/src/components/Field/Field.jsx
@@ -1,6 +1,8 @@
 import Card from 'react-bootstrap/Card';
+import { generateField } from '../../utils';
 
 function Field() {
+  console.log(generateField());
   return (
     <Card style={{ width: '20rem' }}>
       <Card.Body>Тут будут карточки</Card.Body>
---
+++ memory-game/src/utils/index.js
@@ -0,0 +1,32 @@
+import { CARD_NAMES } from "../components/constants";
+
+export function generateField() {
+    // the size of the field is 4
+
+    // pick 8 names from images randomly
+
+    const shuffledCardNames = shuffleArray(Object.keys(CARD_NAMES));
+    const pickedNames = shuffledCardNames.slice(0, 8);
+
+    const duplicatedNames = [...pickedNames, ...pickedNames];
+
+    const shuffledDuplicatedNames = shuffleArray(duplicatedNames);
+
+    // create 2d array
+
+    const field = [shuffledDuplicatedNames.slice(0, 4),
+    shuffledDuplicatedNames.slice(4, 8),
+    shuffledDuplicatedNames.slice(8, 12),
+    shuffledDuplicatedNames.slice(12, 16)];
+    
+    return field;
+}
+
+function shuffleArray(unshuffled) {
+    let shuffled = unshuffled
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+    
+    return shuffled;
+}
