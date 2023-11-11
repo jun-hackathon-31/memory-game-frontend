26aec898b73b198eb7d88c84431445129f5ce458
RadostevaT <radostevataisia@gmail.com>
2023-11-11 16:41:09 +0500

    added react-bootstrap

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import {Button} from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-      <h1>Hello! It&laquo;s memory game</h1>
+      <h1>Hello! It&lsquo;s memory game</h1>
+      <Button>Test</Button>
     </>
   )
 }
